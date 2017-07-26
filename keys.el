e7855fd666e7cbb25871db0423a0023673db053e
Nick Drozd <nicholasdrozd@gmail.com>
2017-07-26 14:48:30 -0500

    Modify defkey to not require quotation marks
    Instead of (defkey "M-s" save-buffer) and (defkey "C-x C-b" ibuffer),
    put (defkey M-s save-buffer) and (defkey (C-x C-b) ibuffer). Is this a
    frivolous and extravagant macro? Maybe. Is it inexcusably lazy to want
    to type keybindings without quotation marks? Maybe.
    
    It would be nice to be able to do all the bindings in one go, as
    in (defkeys M-s save-buffer (C-x C-b) ibuffer).

--- keys.el
+++ keys.el
@@ -4,37 +4,44 @@
 ;; names should be saved, use `defbind`.
 (defmacro defkey (keyb func)
   "Assign a function to a keybinding."
-  `(global-set-key
-    (kbd ,keyb)
-    (if (listp ',func)
-	(lambda () (interactive) ,func)
-      ',func)))
+  (let ((keyb-string
+	 (if (listp keyb)
+	     (apply 'concat
+		    (mapcar (lambda (kb)
+			      (concat (symbol-name `,kb) " "))
+			    keyb))
+	   (symbol-name `,keyb))))
+    `(global-set-key
+      (kbd ,keyb-string)
+      ,(if (listp func)
+	   `(lambda () (interactive) ,func)
+	 `',func))))
 
 ;; C-x C-s? yeah right
-(defkey "M-s" save-buffer)
+(defkey M-s save-buffer)
 
 ;; ibuffer is better than buffer-list
-(defkey "C-x C-b" ibuffer)
+(defkey (C-x C-b) ibuffer)
 
 ;; Make regexp search the default
-(defkey "C-s" isearch-forward-regexp)
-(defkey "C-r" isearch-backward-regexp)
-(defkey "C-M-s" isearch-forward)
-(defkey "C-M-r" isearch-backward)
+(defkey C-s isearch-forward-regexp)
+(defkey C-r isearch-backward-regexp)
+(defkey C-M-s isearch-forward)
+(defkey C-M-r isearch-backward)
 
 ;; C-x o? please
-(defkey "C-<right>" other-window)
-(defkey "C-<left>" (other-window -1))
-(defkey "C-." other-window)
-(defkey "C-," (other-window -1))
+(defkey C-<right> other-window)
+(defkey C-<left> (other-window -1))
+(defkey C-. other-window)
+(defkey C-\, (other-window -1))
 
 ;; Alternative to M-x (sites.google.com/site/steveyegge2/effective-emacs)
-(defkey "C-c C-m" execute-extended-command)
+(defkey (C-c C-m) execute-extended-command)
 
-(defkey "C-c q" query-replace-regexp)
+(defkey (C-c q) query-replace-regexp)
 
-(defkey "C-w" backward-kill-word)
-(defkey "C-x C-k" kill-region)
+(defkey C-w backward-kill-word)
+(defkey (C-x C-k) kill-region)
 
 
 (defmacro defbind (keyb name args &rest body)
