9db2228b4db5e1670c7d3776a0753f355c19a889
Nick Drozd <nicholasdrozd@gmail.com>
2017-11-07 07:59:24 -0600

    Add scraps.el
    At some point I'll get around to organizing this stuff better.

---
+++ scraps.el
@@ -0,0 +1,33 @@
+(defun increment-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0-9")
+  (or (looking-at "[0-9]+")
+      (error "No number at point"))
+  (replace-match (number-to-string (1+ (string-to-number (match-string 0))))))
+
+(defun decrement-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0-9")
+  (or (looking-at "[0-9]+")
+      (error "No number at point"))
+  (replace-match (number-to-string (1- (string-to-number (match-string 0))))))
+
+;; yas
+;; PropertySchema.${1:$(upcase yas-text)}: '$1_value',
+;; $0
+
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+
+
+(defun to-underscore ()
+  (interactive)
+  (replace-regexp "\\([A-Z]\\)" "_\\1" nil (region-beginning) (region-end))
+  (downcase-region (region-beginning) (region-end)))
